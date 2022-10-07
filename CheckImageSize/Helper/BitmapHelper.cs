45c0cb3a0d3e75e6d9e5591f385228796a768965
Yan Amorelli <yan.amorelli@trescon.com.br>
2022-10-07 16:23:38 -0300

    Adicionar arquivos de projeto.

---
+++ CheckImageSize/Helper/BitmapHelper.cs
@@ -0,0 +1,41 @@
+﻿using System.Drawing;
+
+namespace CheckImageSize.Helper
+{
+    internal static class BitmapHelper
+    {
+        internal static Bitmap Base64StringToBitmap(this string base64String)
+        {
+            Bitmap bmpReturn = null;
+
+            byte[] byteBuffer = Convert.FromBase64String(base64String);
+            MemoryStream memoryStream = new MemoryStream(byteBuffer);
+
+            memoryStream.Position = 0;
+
+            bmpReturn = (Bitmap)Bitmap.FromStream(memoryStream);
+
+            memoryStream.Close();
+            memoryStream = null;
+            byteBuffer = null;
+
+            return bmpReturn;
+        }
+
+        internal static bool IsValid(Bitmap image, long fileSize)
+        {
+            if (image.Width > 1280 || image.Height > 220)
+            {
+                return false;
+            }
+
+            // 1mb = 1024000bytes
+            if (fileSize > 1024000)
+            {
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
---
+++ CheckImageSize/Program.cs
@@ -0,0 +1,11 @@
+﻿using CheckImageSize.Helper;
+using System.Drawing;
+
+var path = Path.GetFullPath("C:/Users/3CON-RJ/Desktop/Tamanho_maior.txt");
+string base64Img = File.ReadAllText(path);
+long fileInfo = new FileInfo(path).Length;
+
+
+Bitmap imgBitmap = BitmapHelper.Base64StringToBitmap(base64Img);
+bool response = BitmapHelper.IsValid(imgBitmap, fileInfo);
+Console.WriteLine(response);
